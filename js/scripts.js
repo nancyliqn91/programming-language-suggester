431b3b546d73f85b30714e863ec486292228b5aa
nancyliqn91 <nancyliqn91@gmail.com>
2023-03-24 11:26:38 -0700

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -12,29 +12,32 @@ function handleChoice(event) {
   // For result
   let result;
   if (answer2 === "yes" ||answer3 ==="yes") {
-    result = "study Python";
+    result = "you should study Python";
   } else if (answer4 === "yes" ||answer5 ==="yes") {
-    result = " study JavaScript";
+    result = " you should study JavaScript";
   } else if (answer2 === "no" ||answer4 === "no") {
-    result = "study Java";
+    result = "you shuld study Java";
   } else if (answer3 === "no" ||answer5 === "no") {
-    result = "Just pick the language you're interested.";
-  } else {
-    return result = "Plese answer questions by type yes or no."
+    result = "just pick the language you're interested.";
+  } else { result = "plese answer questions by type yes or no."
   }
 
-  document.getElementById("output").innerText = name +" you shold " + result;
+  document.getElementById("output").innerText = name +" "+ result;
 }
+ 
+
  // For reset
- let resetBtn = document.getElementById("reset");
-//  resetBtn.addEventListener("click", function() {
-  // story.setAttribute("class", "hidden");
-  // document.getElementById("input2").value = null;
-  // document.getElementById("input3").value = null;
-  // document.getElementById("input4").value = null;
-  // document.getElementById("input5").value = null;
-// });
+
 window.addEventListener("load", function() {
   const form = document.getElementById("suggester");
+  let resetBtn = document.getElementById("reset");
   form.addEventListener("submit", handleChoice);
+  resetBtn.addEventListener("click", function() {
+    // story.setAttribute("class", "hidden");
+    document.getElementById("name").value = null;
+    document.getElementById("input2").value = null;
+    document.getElementById("input3").value = null;
+    document.getElementById("input4").value = null;
+    document.getElementById("input5").value = null;
+  });
 });
