d6a3cd054afb30de8245e9fb536f144eda49004b
nancyliqn91 <nancyliqn91@gmail.com>
2023-03-24 10:55:08 -0700

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@
 function handleChoice(event) {
   // For input
   event.preventDefault();
-  const name = document.getElementById("input1").value;
+  const name = document.getElementById("name").value;
   const answer2 = document.getElementById("input2").value;
   const answer3 = document.getElementById("input3").value;
   const answer4 = document.getElementById("input4").value;
@@ -12,28 +12,28 @@ function handleChoice(event) {
   // For result
   let result;
   if (answer2 === "yes" ||answer3 ==="yes") {
-    result = "Python";
+    result = "study Python";
   } else if (answer4 === "yes" ||answer5 ==="yes") {
-    result = "JavaScript";
+    result = " study JavaScript";
   } else if (answer2 === "no" ||answer4 === "no") {
-    result = "Java";
+    result = "study Java";
   } else if (answer3 === "no" ||answer5 === "no") {
     result = "Just pick the language you're interested.";
   } else {
-    return "Plese answer questions by type yes or no."
+    return result = "Plese answer questions by type yes or no."
   }
 
-  document.getElementById("output").innerText = name +", you shold study " + result;
+  document.getElementById("output").innerText = name +" you shold " + result;
 }
  // For reset
  let resetBtn = document.getElementById("reset");
- resetBtn.addEventListener("click", function() {
+//  resetBtn.addEventListener("click", function() {
   // story.setAttribute("class", "hidden");
-  document.getElementById("input2").value = null;
-  document.getElementById("input3").value = null;
-  document.getElementById("input4").value = null;
-  document.getElementById("input5").value = null;
-});
+  // document.getElementById("input2").value = null;
+  // document.getElementById("input3").value = null;
+  // document.getElementById("input4").value = null;
+  // document.getElementById("input5").value = null;
+// });
 window.addEventListener("load", function() {
   const form = document.getElementById("suggester");
   form.addEventListener("submit", handleChoice);
