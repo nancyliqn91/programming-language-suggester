8cc64c2a12ef74698220f60aba94d9cd23ad4aa9
nancyliqn91 <nancyliqn91@gmail.com>
2023-03-24 18:06:22 -0700

    It will create nice suggestion.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,19 @@ const python = "You should study Python. Python is easy to learn,versatile, and 
 const javaScript = " You should study JavaScript. JS is used for web development, easy to integrate with HTML and CSS. Also, it's widely used.";
 const java = "You shuld study Java. Java is object-oriented, platform-indepedent, and used in enterprise applications.";
 const random = "Just pick the language you're interested. All of the above languages are popular programming languages used for a wide range of applications.";
+const csharp = "You chose mostly c's, you should definitely study C#!"
+const ruby = "You chose mostly b's, you should definitely study Ruby!"
+// For calculate checked box
+function number(id){
+  const num = document.getElementById(id);
+  if (num.checked) {
+    const check = 1;
+    return check;
+  } else {
+    const uncheck = 0;
+    return uncheck;
+  }
+}
 // User Interface Logic
 function handleQestion(event) {
   // For input of question section
@@ -34,38 +47,34 @@ function handleInterests(event) {
   conclusion2.removeAttribute("class");
   event.preventDefault();
   const name = document.getElementById("name").value;
-  const int1 = parseInt(document.getElementById("coding").value);
-  const int2 = parseInt(document.getElementById("calculation").value);
-  const int3 = parseInt(document.getElementById("color").value);
-  const int4 = parseInt(document.getElementById("book").value);
-  const int5 = parseInt(document.getElementById("bot").value);
-  const int6 = parseInt(document.getElementById("art").value);
+  const int1 = number("coding");
+  const int2 = number("calculation");
+  const int3 = number("color");
+  const int4 = number("book");
+  const int5 = number("bot");
+  const int6 = number("art");
   const a = int1 + int2 + int3;
   const b = int4 + int5;
   const c = int6;
   // For result
   let interests;
-  if (a>b) {
-    interests = python;
-  } else if (a<b) {
-    interests = javaScript;
-  } else if (a === b && b!= c) {
-    interests = java;
-  } else if (a === b === c) {
-    interests = random;
-  } else { interests = "Plese check at least one box above."
+  if (a > b > 0) {
+    interests = csharp;
+  } else if (0 < a < b) {
+    interests = ruby;
+  } else { interests = random;
   }
 
   document.getElementById("output2").innerText = name +" "+ interests;
 }
 
 window.addEventListener("load", function() {
-  const form = document.getElementById("suggester");
   // For question section
+  const form1 = document.getElementById("suggester1");
   let resetBtn1 = document.getElementById("reset1");
   let conclusion1 = document.getElementById("conclusion1");
-  form.addEventListener("submit", handleQestion);
-  form.addEventListener("submit", function() {
+  form1.addEventListener("submit", handleQestion);
+  form1.addEventListener("submit", function() {
     resetBtn1.removeAttribute("class");
   }); 
   resetBtn1.addEventListener("click", function() {
@@ -76,11 +85,13 @@ window.addEventListener("load", function() {
     document.getElementById("input4").value = null;
     document.getElementById("input5").value = null;
   });
+
   // For interests section
+  const form2 = document.getElementById("suggester2");
   let resetBtn2 = document.getElementById("reset2");
   let conclusion2 = document.getElementById("conclusion2");
-  form.addEventListener("submit", handleInterests);
-  form.addEventListener("submit", function() {
+  form2.addEventListener("submit", handleInterests);
+  form2.addEventListener("submit", function() {
     resetBtn2.removeAttribute("class");
   }); 
   resetBtn2.addEventListener("click", function() {
